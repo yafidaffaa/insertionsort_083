123f753daeac9f037ee9260fa4f366b35864488e
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-24 14:47:52 +0700

    membuat perulangan dengan while, jika n <= 20 maka break dan else array maksimal adalah 20.

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 element.\n";
+		}
+	}
+}
