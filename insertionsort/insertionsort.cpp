4b814c9ca073e0c0eeef309dc83ac6358ff0c901
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-24 14:58:02 +0700

    membuat fungsion insertsort dan menambahkan variabel temp dan variabel j.

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -28,3 +28,19 @@ void input() {
 		cin >> arr[i];
 	}
 }
+
+void insertsort() {
+	int temp;
+	int j;
+
+	for (int i = 1; i < n; i++) {
+		temp = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+	}
+}
